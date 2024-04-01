828822d6d763bdff6e21f67041d0222d31ad743f
Willy Yeremi <willyyeremi@gmail.com>
2024-04-01 15:47:59 +0700

    build: adding dim_time script to script_postgresql.sql

--- script_postgresql.sql
+++ script_postgresql.sql
@@ -1420,8 +1420,7 @@ create table public.dim_date as (
 			,'1970-01-01'::DATE + SEQUENCE.DAY AS datum
 		FROM 
 			GENERATE_SERIES(0, 47846) AS SEQUENCE (DAY)) as list_date
-)
-;
+);
 comment on column public.dim_date."date_dim_id" is 'unique identifier for each row';
 comment on column public.dim_date."date_actual" is 'actual date in iso 8601 format, yyyy-mm-dd';
 comment on column public.dim_date."epoch" is 'epoch value for each date';
@@ -1518,7 +1517,40 @@ comment on column public.dim_date."total_week_of_year_5" is 'total week based on
 comment on column public.dim_date."is_weekend" is 'weekend on saturday and sunday';
 comment on column public.dim_date."is_leap_year" is 'iso 8601 leap year calculation';
 -- section 1.2: script for creating table dimension time
-
+drop table if exists public.dim_time;
+create table public.dim_time as(
+	select
+		id
+		,datum as actual_time_1
+		,TO_CHAR(datum, 'HH12:MI:SS AM') as actual_time_2
+		,EXTRACT(EPOCH FROM datum)::bigint AS epoch
+		,to_char(datum,'HH24')::int as hour_1
+		,to_char(datum,'HH12')::int as hour_2
+		,to_char(datum,'mi')::int as "minute"
+		,to_char(datum,'ss')::int as "second"
+		,to_char(datum,'AM') as "meridiem_indicator_1"
+		,to_char(datum,'am') as "meridiem_indicator_2"
+		,to_char(datum,'A.M.') as "meridiem_indicator_3"
+		,to_char(datum,'a.m.') as "meridiem_indicator_4"
+	FROM
+		(SELECT 
+			"second" + 1 as "id"
+			,time '00:00:01' + (SEQUENCE.second||'seconds')::interval AS datum
+		FROM 
+			GENERATE_SERIES(0, 86400) AS SEQUENCE(second)) as list_second
+);
+comment on column public.dim_time."id" is 'unique identifier for each row';
+comment on column public.dim_time."actual_time_1" is 'time with 24 hour format and hh:mi:ss format';
+comment on column public.dim_time."actual_time_2" is 'time with 12 hour format and hh:mi:ss format';
+comment on column public.dim_time."epoch" is 'epoch value for each date';
+comment on column public.dim_time."hour_1" is 'hour from 24 hour time format';
+comment on column public.dim_time."hour_2" is 'hour from 12 hour time format';
+comment on column public.dim_time."minute" is 'minute from time';
+comment on column public.dim_time."second" is 'second from time';
+comment on column public.dim_time."meridiem_indicator_1" is 'meridiem indicator with uppercase and without dot';
+comment on column public.dim_time."meridiem_indicator_2" is 'meridiem indicator with lowercase and without dot';
+comment on column public.dim_time."meridiem_indicator_3" is 'meridiem indicator with uppercase and with dot';
+comment on column public.dim_time."meridiem_indicator_4" is 'meridiem indicator with lowercase and with dot';
 
 -- section 2: script to preview data
 -- section 2.1: preview for table dimension date
@@ -2942,3 +2974,22 @@ FROM
 		GENERATE_SERIES(0, 47846) AS SEQUENCE (DAY)) as list_date
 ;
 -- section 2.2: preview for table dimension time
+select
+	id
+	,datum as actual_time_1
+	,TO_CHAR(datum, 'HH12:MI:SS AM') as actual_time_2
+	,to_char(datum,'HH24')::int as hour_1
+	,to_char(datum,'HH12')::int as hour_2
+	,to_char(datum,'mi')::int as "minute"
+	,to_char(datum,'ss')::int as "second"
+	,to_char(datum,'AM') as "meridiem_indicator_1"
+	,to_char(datum,'am') as "meridiem_indicator_2"
+	,to_char(datum,'A.M.') as "meridiem_indicator_3"
+	,to_char(datum,'a.m.') as "meridiem_indicator_4"
+FROM
+	(SELECT 
+		"second" + 1 as "id"
+		,time '00:00:01' + (SEQUENCE.second||'seconds')::interval AS datum
+	FROM 
+		GENERATE_SERIES(0, 86400) AS SEQUENCE(second)) as list_second
+;
